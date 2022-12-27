31abd3f4bcb48ed014fac2256fd72186b11b62d0
Linek Reis <linekreis@hotmail.com>
2022-12-27 10:03:46 -0500

    Add files via upload

--- sextodesafio/Procedures.sql
+++ sextodesafio/Procedures.sql
@@ -1,22 +1,37 @@
-DESC DEPARTAMENT;
+desc departament;
 use company;
 /* Thinking in how the company will grow, at some point we'll need
 	new departaments */
     
 drop procedure new_departament;
 
-DELIMITER //
-create procedure new_departament(
-	in Dname_p varchar(30),
-	in Dnumber_p int,
-	in Dept_create_date_p date
-)
+delimiter //
+create procedure new_departament(Dname_p varchar(30),Dnumber_p int,Dept_create_date_p date)
 	begin
-			insert into departament (Dname,Dnumber,Dept_create_date) 
-				values(
-					Dname_p,Dnumber_p,Dept_create_date_p
-				);
+		insert into departament (Dname,Dnumber,Dept_create_date) values(Dname_p,Dnumber_p,Dept_create_date_p);
 	end //
-DELIMITER ;
+delimiter ;
 
 call new_departament('Financeiro',7,null);
+
+delimiter ||
+create procedure data_manipulation(num int, dpt_num int,in dept_loc varchar(40))
+begin
+    
+	case num
+		when 1 then
+			insert into departament(Dnumber,Dname,Dept_create_date) values (dpt_num,dept_loc,current_date());
+            select * from company.departament;
+		when 2 then 
+			delete from departament where Dnumber = dept_num;
+            select * from company.departament;
+		when 3 then
+			update departament set Dname = 'Research' where Dnumber = dept_num;
+            select * from company.departament;
+		else
+			select * from company.departament;
+	END CASE;
+end ||
+delimiter ;
+drop procedure data_manipulation;
+call data_manipulation(null,null,'');
